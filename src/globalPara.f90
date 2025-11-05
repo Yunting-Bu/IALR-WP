6aeb51c19d1455cc3927cdf6d9b3cdce5b99d1c9
Yunting-Bu <byt@mail.sdu.edu.cn>
2025-11-05 10:09:15 +0800

    Add module gPara 2025.11.05

---
+++ src/globalPara.f90
@@ -0,0 +1,219 @@
+module gPara
+    use, intrinsic :: iso_fortran_env, only : f8 => real64
+    implicit none
+    public
+
+    private :: getMass, diatomParity
+
+!> ========== Constants ==========
+
+    real(f8), parameter :: au2ev = 27.211386245988_f8
+    real(f8), parameter :: ev2au = 1.0_f8 / au2ev
+    real(f8), parameter :: au2cm = 219474.6313705_f8
+    real(f8), parameter :: cm2au = 1.0_f8 / au2cm 
+    real(f8), parameter :: au2K = 315777.663_f8
+    real(f8), parameter :: K2au = 1.0_f8 / au2K
+    real(f8), parameter :: amu2au = 1822.888486209_f8
+    real(f8), parameter :: pi = 4.0_f8*atan(1.0_f8)
+   
+!> ========== Global Parameters ==========
+
+    type :: initWP_class
+        integer :: j0, v0, l0, Jtot, tpar, jpar
+        real(f8) :: Zc, delta, Ec
+        integer :: initPES
+        integer :: Kmax, Kmin, K0, jmin, jinc 
+    end type initWP_class
+
+    type :: IALR_class
+       integer :: nZtot, nZasy, nZint
+       integer :: vint, jint, vasy, jasy 
+       real(f8) :: Z_range(2), r_range(2)
+       real(f8), allocatable :: Ztot(:), Zasy(:), Zint(:) 
+    end type IALR_class
+
+    type :: Vabs_class
+        real(f8) :: Casy, Clr, Cr 
+        real(f8) :: Zasy_range(2), Zlr_range(2), rabs_range(2)
+        real(f8), allocatable :: Zasy(:), ZLr(:), rabs(:)
+        real(f8), allocatable :: Fasy(:), Flr(:), Fabs(:) 
+    end type Vabs_class
+
+    type :: ine_class
+        integer :: vmax, jmax
+        real(f8) :: project_Zine
+        integer :: T_ine
+    end type ine_class
+
+    type :: channel1_class
+        integer :: nrp, vpmax, jpmax, jpar, midcoor 
+        real(f8) :: rp_range(2), rinf 
+        integer :: jmin, jinc
+        real(f8), allocatable :: rp(:)
+    end type channel1_class
+
+    type :: channel2_class
+        integer :: nrp, vpmax, jpmax, jpar, midcoor 
+        real(f8) :: rp_range(2), rinf 
+        integer :: jmin, jinc
+        real(f8), allocatable :: rp(:)
+    end type channel2_class
+
+!> ========== Parameters from input file ==========
+
+    integer :: reactChannel, IF_inelastic, nPES, energyUnit
+    integer :: T_tot, timeStep, timePrint
+    integer :: nEtot  
+    real(f8) :: E_range(2), dE, Etot
+    real(f8) :: atomMass(3), massBC, massTot
+    character(len=2) :: Atoms(3), potentialType
+    real(f8) :: energyUnitTrans(4) = [cm2au, ev2au, K2au, 1.0_f8]
+
+    type(initWP_class) :: initWP
+    type(IALR_class) :: IALR 
+    type(Vabs_class) :: Vabs 
+    type(ine_class) :: ine 
+    type(channel1_class) :: channel1
+    type(channel2_class) :: channel2 
+
+!> ========== Namelists ==========
+
+    namelist /task/ reactChannel, IF_inelastic, Atoms, nPES, energyUnit, potentialType
+    namelist /energy/ E_range, dE
+    namelist /initWP/ initWP
+    namelist /IALR/ IALR
+    namelist /Vabs/ Vabs
+    namelist /propagation/ T_tot, timeStep, timePrint
+    namelist /inelastic/ ine
+    namelist /channel1/ channel1
+    namelist /channel2/ channel2
+
+
+contains
+
+    subroutine initPara()
+        implicit none
+        integer :: iEtot
+        real(f8) :: temp
+
+!> ========== Read parameters from input file ==========
+
+        open(unit=233, file="ABC.inf", status='old')
+        read(233, nml=task) 
+        read(233, nml=energy) 
+        read(233, nml=initWP)
+        read(233, nml=IALR)
+        read(233, nml=Vabs)
+        read(233, nml=propagation)
+        read(233, nml=channel1)
+
+        if (reactChannel == 2) read(233, nml=channel2)
+        if (IF_inelastic == 0) read(233, nml=inelastic) 
+
+        close(233)
+
+!> ========== Construct collision energy ==========
+
+        nEtot = nint((E_range(2) - E_range(1))/dE)
+        allocate(Etot(nEtot))
+        temp = E_range(1)
+        do iEtot = 1, nEtot 
+            Etot(iEtot) = temp + dE 
+            if (Etot(iEtot) > E_range(2)) then
+                Etot(iEtot) = E_range(2)
+            end if 
+            !> convert to au
+            Etot(iEtot) = Etot(iEtot) * energyUnitTrans(energyUnit)
+        end do
+
+        if (initWP%tpar == 1) then 
+            initWP%Kmin = 0
+        else if (initWP%tpar == -1) then 
+            initWP%Kmin = 1
+        else 
+            write(*,*) "Error: total parity tpar must be 1 or -1."
+            write(*,*) "POSITION: globalPara.f90, subroutine initPara()"
+            stop
+        end if
+
+        call getMass()
+        call diatomParity()
+
+    end subroutine initPara
+
+    subroutine getMass()
+        implicit none
+        integer, parameter :: nSupportedElements = 10
+        character(len=2), parameter :: elemSymbol(nSupportedElements) = &
+            ['H ', 'D', 'He', 'Li', 'N ', 'O ', 'F ', 'S', 'Cl', 'Ar']
+        real(f8), parameter :: elemMass(nSupportedElements) = &
+            [1.00784_f8, 2.01410_f8, 4.002602_f8, 6.938_f8, 14.003074_f8, &
+             15.99491461957_f8, 18.998403163_f8, 32.06_f8, 34.968852682_f8, 39.9623831237_f8]
+        
+        integer :: iatm, ipos
+
+        do iatm = 1, 3 
+            ipos = findloc(elemSymbol, Atoms(iatm))
+            if (ipos == 0) then
+                write(*,*) "Error: Unsupported element ", Atoms(iatm)
+                write(*,*) "POSITION: globalPara.f90, subroutine getMass()"
+                stop
+            else
+                atomMass(iatm) = elemMass(ipos)*amu2au
+            end if
+        end do
+
+        !> Reduce mass of BC : mB*mC/(mB+mC)
+        !> Reduce mass of ABC : mA*(mB+mC)/(mA+mB+mC)
+        massBC = atomMass(2)*atomMass(3)/(atomMass(2)+atomMass(3))
+        massTot = atomMass(1)*(atomMass(2)+atomMass(3))/(atomMass(1)+atomMass(2)+atomMass(3))
+
+    end subroutine getMass
+    
+    subroutine diatomParity()
+        implicit none
+        !> jpar = -1, 0, 1, jmin = 1, 0, 0, jinc = 2, 0, 2
+        integer :: jmin(-1:1) = [1, 0, 0]
+        integer :: jinc(-1:1) = [2, 0, 2]
+        real(f8) :: eps 
+        
+        eps = 100.0_f8 * epsilon(atomMass(1))
+
+        if (abs(atomMass(2)-atomMass(3)) < eps) then
+            !> B and C are identical atoms
+            if (initWP%jpar /= 0) then
+                write(*,*) "Error: For identical atoms, BC parity jpar must be 0."
+                write(*,*) "POSITION: globalPara.f90, subroutine diatomParity()"
+                stop
+            end if
+        end if
+        initWP%jmin = jmin(initWP%jpar)
+        initWP%jinc = jinc(initWP%jpar)
+
+        if (abs(atomMass(1)-atomMass(2)) < eps) then
+            !> A and B are identical atoms
+            if (channel1%jpar /= 0) then
+                write(*,*) "Error: For identical atoms, AB parity jpar must be 0."
+                write(*,*) "POSITION: globalPara.f90, subroutine diatomParity()"
+                stop
+            end if
+        end if
+        channel1%jmin = jmin(channel1%jpar)
+        channel1%jinc = jinc(channel1%jpar)
+
+        if (reactChannel == 2) then
+            if (abs(atomMass(1)-atomMass(3)) < eps) then
+                !> A and C are identical atoms
+                if (channel2%jpar /= 0) then
+                    write(*,*) "Error: For identical atoms, AC parity jpar must be 0."
+                    write(*,*) "POSITION: globalPara.f90, subroutine diatomParity()"
+                    stop
+                end if
+            end if
+            channel2%jmin = jmin(channel2%jpar)
+            channel2%jinc = jinc(channel2%jpar)
+        end if
+
+    end subroutine diatomParity
+
+end module gPara
