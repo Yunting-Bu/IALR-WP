29137bf4a43f95fcbd311bd42015562e7543aebc
Yunting-Bu <byt@mail.sdu.edu.cn>
2025-11-14 15:08:01 +0800

    Add RotMat 2025.11.14

--- src/propagation.f90
+++ src/propagation.f90
@@ -8,8 +8,43 @@ module propMod
 
 contains 
 
+!> ------------------------------------------------------------------------------------------------------------------ <!
     subroutine getRotMat()
         implicit none
+        real(f8) :: fact, jeigen
+        integer :: ir, j
         
-        allocatable
+        allocate(rotMat(IALR%nr_int,IALR%jint))
+        do ir = 1, IALR%nr_int 
+            fact = 1.0_f8/(2.0_f8*massTot*r_Int(ir)**2)
+            do j = 1, IALR%jint
+                jeigen = real(j*(j+1),f8)
+                rotMat(ir,j) = fact*jeigen
+            end do 
+        end do 
+
+    end subroutine getRotMat
+!> ------------------------------------------------------------------------------------------------------------------ <!
+
+!> ------------------------------------------------------------------------------------------------------------------ <!
+    real(f8) function lambdaPlus(J,K) result(res)
+        implicit none
+        integer, intent(in) :: J, K
+
+        res = dsqrt(J*(J+1.0_f8) - K*(K+1.0_f8))
+        return
+
+    end function lambdaPlus
+!> ------------------------------------------------------------------------------------------------------------------ <!
+
+!> ------------------------------------------------------------------------------------------------------------------ <!
+    real(f8) function lambdaMinus(J,K) result(res)
+        implicit none
+        integer, intent(in) :: J, K
+
+        res = dsqrt(J*(J+1.0_f8) - K*(K-1.0_f8))
+        return
+
+    end function lambdaPlus
+!> ------------------------------------------------------------------------------------------------------------------ <!
     
